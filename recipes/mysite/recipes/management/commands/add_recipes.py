ec9c6df37cc432ce0b75f15e05e5de95c7f4ef14
Tim Jacomb <timjacomb1@gmail.com>
2019-02-15 22:34:07 +0000

    if your json is invalid you now get an error message dummy

--- recipes/mysite/recipes/management/commands/add_recipes.py
+++ recipes/mysite/recipes/management/commands/add_recipes.py
@@ -6,42 +6,43 @@ import json
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		with open('recipes.json') as f:
-			data = json.load(f)
-			recipes = data["recipes"]
-			for recipe in recipes:
-				tag_ids = []
-				if 'tags' in recipe:
-					for tag in recipe['tags']:
-						if Tag.objects.filter(name=tag):
-							print('Tag ' + tag + ' already exists, skipping')
-							tag_ids.append(Tag.objects.filter(name=tag)[0].id)
-						else:
-							print('Adding new tag for ' + tag)
-							new_tag = Tag(name=tag)
-							new_tag.save()
-							tag_ids.append(new_tag.id)
-				if Recipe.objects.filter(title=recipe['title']):
-					print('Recipe ' + recipe['title'] + ' already exists, updating')
-					update_recipe = Recipe.objects.filter(title=recipe['title'])[0]
-					string_ingredients = '{"ingredients":' + json.dumps(recipe['ingredients']) + '}'
-					string_steps = '{"steps":' + json.dumps(recipe['steps']) + '}'
-
-					update_recipe.title = recipe['title']
-					update_recipe.url = recipe['url']
-					update_recipe.ingredients = string_ingredients
-					update_recipe.steps = string_steps
-					update_recipe.save()
-
-					tags_blah = recipe['tags']
-
-					if tags_blah:
-						update_recipe.tags.set(tag_ids)
-				else:
-					print('Adding new recipe for ' + recipe['title'])
-					string_ingredients = '{"ingredients":' + json.dumps(recipe['ingredients']) + '}'
-					string_steps = '{"steps":' + json.dumps(recipe['steps']) + '}'
-					r = Recipe(title=recipe['title'], pub_date=timezone.now(), url=recipe['url'], ingredients=string_ingredients, 
-						steps=string_steps, image=recipe['image'])
-					r.save()
+			try:
+				data = json.load(f)
+				recipes = data["recipes"]
+				for recipe in recipes:
+					tag_ids = []
 					if 'tags' in recipe:
-						r.tags.set(tag_ids)
+						for tag in recipe['tags']:
+							if Tag.objects.filter(name=tag):
+								print('Tag ' + tag + ' already exists, skipping')
+								tag_ids.append(Tag.objects.filter(name=tag)[0].id)
+							else:
+								print('Adding new tag for ' + tag)
+								new_tag = Tag(name=tag)
+								new_tag.save()
+								tag_ids.append(new_tag.id)
+					if Recipe.objects.filter(title=recipe['title']):
+						print('Recipe ' + recipe['title'] + ' already exists, updating')
+						update_recipe = Recipe.objects.filter(title=recipe['title'])[0]
+						string_ingredients = '{"ingredients":' + json.dumps(recipe['ingredients']) + '}'
+						string_steps = '{"steps":' + json.dumps(recipe['steps']) + '}'
+
+						update_recipe.title = recipe['title']
+						update_recipe.url = recipe['url']
+						update_recipe.ingredients = string_ingredients
+						update_recipe.steps = string_steps
+						update_recipe.save()
+
+						if 'tags' in recipe:
+							update_recipe.tags.set(tag_ids)
+					else:
+						print('Adding new recipe for ' + recipe['title'])
+						string_ingredients = '{"ingredients":' + json.dumps(recipe['ingredients']) + '}'
+						string_steps = '{"steps":' + json.dumps(recipe['steps']) + '}'
+						r = Recipe(title=recipe['title'], pub_date=timezone.now(), url=recipe['url'], ingredients=string_ingredients, 
+							steps=string_steps, image=recipe['image'])
+						r.save()
+						if 'tags' in recipe:
+							r.tags.set(tag_ids)
+			except ValueError as e:
+				print("Invalid JSON, please use an online formatter silly.")
