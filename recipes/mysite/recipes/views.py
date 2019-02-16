aab5ca58c13f604bd68a035c002bc98ea92c06cc
Tim Jacomb <timjacomb1+github@gmail.com>
2019-02-16 10:09:44 +0000

    Format

--- recipes/mysite/recipes/views.py
+++ recipes/mysite/recipes/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Tag, Recipe
-from django.http import HttpResponseRedirect
-from .forms import RecipeForm
 import json
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .forms import RecipeForm
+from .models import Tag, Recipe
+
 
 # Create your views here.
 def index(request):
@@ -15,59 +17,65 @@ def index(request):
     queryset_list = Recipe.objects.all()
     query = request.GET.get("q")
     if query:
-    	queryset_list = queryset_list.filter(title__icontains=query)
+        queryset_list = queryset_list.filter(title__icontains=query)
 
-    context = {'latest_recipes_list': latest_recipes_list, 'tags_list': tags_list, 'this_week': this_week, 'next_week': next_week, 
-    'query': query, 'queryset_list': queryset_list}
+    context = {'latest_recipes_list': latest_recipes_list, 'tags_list': tags_list, 'this_week': this_week,
+               'next_week': next_week,
+               'query': query, 'queryset_list': queryset_list}
     return render(request, 'recipes/index.html', context)
 
-def detail(request, recipe_id):
-	recipe = get_object_or_404(Recipe, id=recipe_id)
-	ingredients  = json.loads(recipe.ingredients)
-	steps = json.loads(recipe.steps)
-	form = RecipeForm(instance=recipe)
 
-	if request.method == 'POST':
-		form = RecipeForm(request.POST, instance=recipe)
-		if form.is_valid():
-			recipe.save()
-			return HttpResponseRedirect('/recipes')
-	
-	context = {'recipe': recipe, 'tags': recipe.tags.all(), 'ingredients': ingredients['ingredients'], 'steps': steps['steps'], 'form': form}
-	return render(request, 'recipes/detail.html', context)
+def detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    ingredients = json.loads(recipe.ingredients)
+    steps = json.loads(recipe.steps)
+    form = RecipeForm(instance=recipe)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            recipe.save()
+            return HttpResponseRedirect('/recipes')
+
+    context = {'recipe': recipe, 'tags': recipe.tags.all(), 'ingredients': ingredients['ingredients'],
+               'steps': steps['steps'], 'form': form}
+    return render(request, 'recipes/detail.html', context)
+
 
 def cooked(request, recipe_id):
-	recipe = get_object_or_404(Recipe, id=recipe_id)
-	ingredients = json.loads(recipe.ingredients)
-	steps = json.loads(recipe.steps)
-	form = RecipeForm(instance=recipe)
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    ingredients = json.loads(recipe.ingredients)
+    steps = json.loads(recipe.steps)
+    form = RecipeForm(instance=recipe)
 
-	if request.method == 'POST':
-		print('save')
-		recipe.cooked_count += 1
-		recipe.this_week = False
-		recipe.save()
-		return HttpResponseRedirect('/recipes')
+    if request.method == 'POST':
+        print('save')
+        recipe.cooked_count += 1
+        recipe.this_week = False
+        recipe.save()
+        return HttpResponseRedirect('/recipes')
+
 
 def move(request):
-	recipes = Recipe.objects.filter(next_week=True)
+    recipes = Recipe.objects.filter(next_week=True)
 
-	if request.method == 'POST':
-		for recipe in recipes:
-			print (recipe)
-			recipe.this_week = True
-			recipe.next_week = False
-			recipe.save()
-		return HttpResponseRedirect('/recipes')
+    if request.method == 'POST':
+        for recipe in recipes:
+            print(recipe)
+            recipe.this_week = True
+            recipe.next_week = False
+            recipe.save()
+        return HttpResponseRedirect('/recipes')
 
 
 def tag(request, tag_id):
-	tag = Tag.objects.get(id=tag_id)
-	recipes = Recipe.objects.filter(tags__in=[tag_id])
-	context = {'tag': tag, 'recipes': recipes}
-	return render(request, 'recipes/tag.html', context)
+    tag = Tag.objects.get(id=tag_id)
+    recipes = Recipe.objects.filter(tags__in=[tag_id])
+    context = {'tag': tag, 'recipes': recipes}
+    return render(request, 'recipes/tag.html', context)
+
 
 def all(request):
-	recipes = Recipe.objects.all()
-	context = {'recipes': recipes}
-	return render(request, 'recipes/all.html', context)
+    recipes = Recipe.objects.all()
+    context = {'recipes': recipes}
+    return render(request, 'recipes/all.html', context)
