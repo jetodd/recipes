10dc3d2799d330b6653d0c3651f31306a8074d3e
Tim Jacomb <timjacomb1@gmail.com>
2019-02-17 13:59:26 +0000

    Added model for shopping item

--- recipes/mysite/recipes/models.py
+++ recipes/mysite/recipes/models.py
@@ -19,3 +19,8 @@ class Recipe(models.Model):
 	cooked_count = models.IntegerField(default=0)
 	def __str__(self):
 		return self.title
+
+class ShoppingItem(models.Model):
+	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.title
