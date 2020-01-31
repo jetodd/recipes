4f81662981308694eed79835e60c72484f68ef75
Tim Jacomb <timjacomb1@gmail.com>
2020-01-31 18:22:37 +0000

    Added search for desktop, doesn't work on mobile

--- recipes/static/recipes/script.js
+++ recipes/static/recipes/script.js
@@ -1,8 +1,10 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 	$('#select_all').click(function() {
-
 		var checkBoxes = $("form input[type='checkbox']")
-		console.log(checkBoxes)
 		checkBoxes.attr( "checked" , true );
+	});
+
+	$('#toggle_search').click(function() {
+		$('#search_form').toggleClass('show');
 	});
 });
